79abb4ff085f4b3817a36a4d43b35597890af372
Dkurtz9 <kurtz.david1995@gmail.com>
2025-08-29 17:01:53 +0200

    Add files via upload

--- suivi_muscu.py
+++ suivi_muscu.py
@@ -153,6 +153,7 @@ elif menu == "Voir mes performances":
                         "exercice": ex,
                         "seance": seance_name,
                         "poids_max": subset.loc[idx_max, "poids"],
+			"Reps_max" : subset.loc[idx_max, "reps"], 
                         "date": subset.loc[idx_max, "date"]
                     }
                     pr_list.append(pr_row)
